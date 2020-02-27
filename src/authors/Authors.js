84b08e6da59eced3ac7b555436b449a6dcfcbf3c
Alison Tomsho <alison.tomsho@gmail.com>
2020-02-26 19:46:57 -0500

    init the project

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Authors";
+import { AuthorDisplay } from "./AuthorDisplay.js";
 
-export default function Authors(){
-    return (
+export default class Authors extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            authors: [
+                {
+                    id:1,
+                    name: "Eric Evans"
+                },
+                {
+                    id:2,
+                    name: "Nicole Forsgren"
+                },
+                {
+                    id:3,
+                    name: "Chip Heath"
+                },
+                {
+                    id:4,
+                    name: "Gary Chapman"
+                },
+                {
+                    id:5,
+                    name: "McGraw Hill"
+                },
+                {
+                    id:6,
+                    name: "Walter Rudin"
+                },
+                {
+                    id:7,
+                    name: "Damodar Gujarati"
+                },
+                {
+                    id:8,
+                    name: "Stephen Ross"
+                }
+            ]
+        }
+
+    };
+    render(){
+        return (
     <div className="Authors">
         <div className="lander">
-            <h1>Authors</h1>
-            <p>Check back soon for Authors!</p>
+            <AuthorDisplay authors={this.state.authors} />
         </div>
     </div>
     );
+}
 }
